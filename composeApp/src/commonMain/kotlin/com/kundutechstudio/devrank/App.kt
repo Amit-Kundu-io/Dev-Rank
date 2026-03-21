7c50957cd48a13eb4e31ab43519df1ec7419c517
Amit Kundu <amitkundu.developer@gmail.com>
2026-03-21 13:41:03 +0530

    setup network module

--- composeApp/src/commonMain/kotlin/com/kundutechstudio/devrank/App.kt
+++ composeApp/src/commonMain/kotlin/com/kundutechstudio/devrank/App.kt
@@ -1,12 +1,23 @@
 package com.kundutechstudio.devrank
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import com.kundutechstudio.devrank.root_navigation.RootNavigation
 import com.kundutechstudio.theme.ui.DevRankTheme
 
 @Composable
 fun App() {
     DevRankTheme {
-        RootNavigation()
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.Black)
+        ) {
+            RootNavigation()
+        }
     }
 }
