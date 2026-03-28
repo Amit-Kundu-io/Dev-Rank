c712c7f43140cbef518d2f36df61043e4be18c56
Amit Kundu <amitkundu.developer@gmail.com>
2026-03-28 10:54:22 +0530

    implement catch for LARGEST_REPOS

--- database/src/commonMain/kotlin/com/kundutechstudio/database/cache_key/CatchKey.kt
+++ database/src/commonMain/kotlin/com/kundutechstudio/database/cache_key/CatchKey.kt
@@ -3,6 +3,7 @@ package com.kundutechstudio.database.cache_key
 enum class CacheKey {
     ACTIVE_REPO,
     BEGINNER_FRIENDLY,
+    LARGEST_REPOS,
     TOP_STARRED,
     TRENDING,
     PROFILE,
--- feature/ranks/src/commonMain/kotlin/com/kundutechstudio/ranks/domain/use_case/get_largest_repos_use_case/GetLargestReposUseCase.kt
+++ feature/ranks/src/commonMain/kotlin/com/kundutechstudio/ranks/domain/use_case/get_largest_repos_use_case/GetLargestReposUseCase.kt
@@ -1,5 +1,7 @@
 package com.kundutechstudio.ranks.domain.use_case.get_largest_repos_use_case
 
+import com.kundutechstudio.database.cache_key.CacheKey
+import com.kundutechstudio.database.domain.repo.CacheDataSourceRepo
 import com.kundutechstudio.network.res.NetworkResult
 import com.kundutechstudio.ranks.domain.dao.RepoItemDAO
 import com.kundutechstudio.ranks.domain.mapper.toRepoItem
@@ -7,33 +9,50 @@ import com.kundutechstudio.ranks.domain.repo.RankRepo
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.IO
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
+import kotlinx.serialization.builtins.ListSerializer
 
 class GetLargestReposUseCase(
-    private val repo: RankRepo
+    private val repo: RankRepo,
+    private val cache: CacheDataSourceRepo
 ) {
+
+    private val serializer = ListSerializer(RepoItemDAO.serializer())
+
     operator fun invoke(): Flow<NetworkResult<List<RepoItemDAO>>> = flow {
 
         emit(NetworkResult.Loading)
 
-        try {
-            val response = repo.getLargestRepos()
+        //Try cache first
+        val cached = cache.read(CacheKey.LARGEST_REPOS, serializer)
 
-            val data: List<RepoItemDAO> = response.items?.mapIndexed { index, item ->
-                item.toRepoItem(rank = index + 1)
-            } ?: emptyList()
-
-            emit(NetworkResult.Success(data))
-
-        } catch (e: Exception) {
-            emit(
-                NetworkResult.Error(
-                    message = e.message ?: "Something went wrong",
-                    cause = e
-                )
-            )
+        if (!cached.isNullOrEmpty()) {
+            emit(NetworkResult.Success(cached))
+            return@flow
         }
 
+        // API call
+        val response = repo.getLargestRepos()
+
+        val data = response.items
+            ?.mapIndexed { index, item ->
+                item.toRepoItem(rank = index + 1)
+            }
+            .orEmpty()
+
+        // Save cache
+        cache.save(CacheKey.LARGEST_REPOS, data, serializer)
+
+        emit(NetworkResult.Success(data))
+
+    }.catch { e ->
+        emit(
+            NetworkResult.Error(
+                message = e.message ?: "Something went wrong",
+                cause = e
+            )
+        )
     }.flowOn(Dispatchers.IO)
 }
