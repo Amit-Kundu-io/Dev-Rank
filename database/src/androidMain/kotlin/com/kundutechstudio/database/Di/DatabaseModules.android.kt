16a0a034018d469167ca338844523e3ea5f23628
Amit Kundu <amitkundu.developer@gmail.com>
2026-08-15 22:56:26 +0530

    add datastore

--- database/src/androidMain/kotlin/com/kundutechstudio/database/Di/DatabaseModules.android.kt
+++ database/src/androidMain/kotlin/com/kundutechstudio/database/Di/DatabaseModules.android.kt
@@ -2,13 +2,17 @@ package com.kundutechstudio.database.Di
 
 import com.kundutechstudio.database.data.cache.FileCache
 import com.kundutechstudio.database.data.repoimpl.CacheDataSourceRepoImpl
+import com.kundutechstudio.database.datastore.DRDataStore
+import com.kundutechstudio.database.datastore.createDataStore
 import com.kundutechstudio.database.domain.repo.CacheDataSourceRepo
 import org.koin.core.module.dsl.bind
 import org.koin.core.module.dsl.singleOf
-import org.koin.dsl.bind
 import org.koin.dsl.module
 
 actual fun databaseModules() = module {
     singleOf(::FileCache)
     singleOf(::CacheDataSourceRepoImpl){bind<CacheDataSourceRepo>()}
+
+    single { createDataStore(get()) }
+    singleOf(::DRDataStore)
 }
