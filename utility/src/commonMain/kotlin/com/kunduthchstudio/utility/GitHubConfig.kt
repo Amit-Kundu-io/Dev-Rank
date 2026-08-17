f5fc77c709c6abfa7f91c1a1f5b0e6454a6088d2
Amit Kundu <amitkundu.developer@gmail.com>
2026-08-18 00:32:19 +0530

    delete

--- utility/src/commonMain/kotlin/com/kunduthchstudio/utility/GitHubConfig.kt
+++ utility/src/commonMain/kotlin/com/kunduthchstudio/utility/GitHubConfig.kt
@@ -15,28 +15,16 @@
 
 package com.kunduthchstudio.utility
 
-/**
- * Run this once in the terminal:
- *
- * git update-index --assume-unchanged "utility/src/commonMain/kotlin/com/kunduthchstudio/utility/GitHubConfig.kt"
- *
- * Why:
- * This tells Git to ignore local changes to this tracked configuration file,
- * so local GitHub credentials/configuration changes are not included
- * when you commit and push changes.
- *
- * Note:
- * This does NOT remove the file from Git or make it secure.
- * Do not store production secrets in this file.
- */
 
 object GitHubConfig {
 
-    const val CLIENT_ID = "YOUR_GITHUB_CLIENT_ID"
-    const val CLIENT_SECRET = "YOUR_GITHUB_CLIENT_secret"
+    //this is key only Demo purpose and any time delete
+    const val CLIENT_ID = "Ov23li3RwvcGyHOV0Pan"
+    const val CLIENT_SECRET = "fc6b569cb13d8ae30371eb6b6c0c67ea504f6fd6"
 
 
     const val AUTHORIZE_URL = "https://github.com/login/oauth/authorize"
     const val TOKEN_URL = "https://github.com/login/oauth/access_token"
     const val REDIRECT_URI = "devrank://oauth/callback"
 }
+
